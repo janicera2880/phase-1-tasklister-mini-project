650bc5eb3c4dc0a5a3d21084bab8dbd2919632c8
Janice Alecha <janiceralecha@gmail.com>
2022-06-04 15:34:47 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+    console.log(event.target['new-task-description'].value)
+    p = document.createElement('p');
+    button = document.createElement('button')
+    button.textContent = 'X'
+    button.addEventListener('click', function(){
+      this.parentNode.remove()
+    })
+    p.textContent = `${event.target['new-task-description'].value} `
+    console.log(p)
+    form.appendChild(p)
+    p.appendChild(button)
+  })
 });
